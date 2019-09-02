8618da02ec111b6993727b9df31b2d4e1abdce9e
kel29 <krista.loven.87@gmail.com>
2019-09-02 13:50:28 -0700

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,31 @@
 class Animal
+    attr_reader :nickname, :species, :zoo
+    attr_accessor :weight
+
+    @@all = []
+
+    def initialize(species, weight, nickname, zoo=nil)
+        @species = species 
+        @weight = weight 
+        @nickname = nickname
+        @zoo = zoo
+        @@all << self 
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def zoo 
+        Zoo.all.select { |z| z.animals }
+    end
+
+    def self.find_by_species(species)
+        self.all.select { |s| s.species == species }
+    end
+
+    def find_me
+        Zoo.all.select { |z| z.animals }
+    end
 
 end
